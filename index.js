a045ad6baef6fe62280ac47e870342784b829de2
rzfd <36652748+rzfd@users.noreply.github.com>
2023-03-17 14:33:01 +0700

    3 files modified

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Toti asu</h1>`;
+let alas = prompt('Masukkan alas');
+let tinggi = prompt('Masukkan tinggi');
+let luas = 0.5 * alas * tinggi;
+
+appDiv.innerHTML = `<h1>Menghitung luas segitiga</h1>`;
+console.log(`luas segitiga = ${luas} cm square`);
+appDiv.innerHTML = `<h1><u>Menghitung luas segitga</u></h1>
+<br>
+Luas Segitiga = 0.5 x ${alas} x ${tinggi}cm = ${luas}
+cm<sup>2</sup>`;
